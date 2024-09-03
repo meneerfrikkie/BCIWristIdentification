335d2c748007dd2eccdede4267a44b0450878a59
Irfaan-Mia <imia154@gmail.com>
2024-09-03 16:49:55 +0200

    add: Error handling for evaluation metrics

--- WorkingFiles/EvaluateModel.m
+++ WorkingFiles/EvaluateModel.m
@@ -5,8 +5,13 @@ function [precision, recall,f1_score] = EvaluateModel(yPred, yTrue)
     fn_1 = sum(yTrue == 1 & yPred == 0); % False Negatives for class 1
 
     precision_1 = tp_1 / (tp_1 + fp_1);
+    if tp_1 + fp_1 == 0
+        precision_1 = 0;
+        f1_score_1 = 0;
+    end 
     recall_1 = tp_1 / (tp_1 + fn_1);
     f1_score_1 = (2*precision_1*recall_1)/(precision_1 + recall_1); 
+
 
     % Compute for Class 0
     tp_0 = sum(yTrue == 0 & yPred == 0); % True Positives for class 0
@@ -16,6 +21,11 @@ function [precision, recall,f1_score] = EvaluateModel(yPred, yTrue)
     precision_0 = tp_0 / (tp_0 + fp_0);
     recall_0 = tp_0 / (tp_0 + fn_0);
     f1_score_0 = (2*precision_0*recall_0)/(precision_0 + recall_0) ;
+        
+    if tp_0 + fp_0 == 0
+        precision_0 = 0;
+        f1_score_0 = 0;
+    end 
 
     % Macro-averaging for precision and recall
     precision = (precision_1 + precision_0) / 2;
